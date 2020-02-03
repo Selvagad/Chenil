80376b019bf63788b2f58a96f056b94db1966d9d
unknown <selvaggio.adam@gmail.com>
2020-02-03 11:41:18 +0100

    Feat : Create the index page

--- chenil/managedog/views.py
+++ chenil/managedog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+import requests
+
+from .models import Dog
 
 # Create your views here.
+def index(request):
+    template = loader.get_template('managedog/index.html')
+    context = {"name":'toto'}
+    return HttpResponse(template.render(context,request))
